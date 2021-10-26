f52dd172e5cc23049eed33a1dd9dda003def8e0e
Collin Van der Vorst <collin@littlemissrobot.com>
2021-10-26 07:00:29 +0200

    Add first JS folder

---
+++ dsps/22-10/assets/js/script.js
@@ -0,0 +1,150 @@
+// These are number
+// var number1 = 10;
+// var number2 = 5;
+
+// var sum = number1 + number2;
+
+// console.log(sum);
+
+
+// text
+// var firstname = "Collin";
+// var familyname = "Van der Vorst";
+// var fullname = firstname + " 'Extremely talented' " + familyname;
+// var tshirtNumber = 9;
+// var nickname = 'goatguy';
+
+// console.log(fullname + tshirtNumber);
+
+
+// var number1 = 10;
+// var number2 = "10";
+
+// console.log(number1 + number2);
+
+// boolean
+// var isMarried = false;
+
+
+
+// Array
+// var characters = ["Harry Potter", "Ron Weasley", "Hermione Granger"];
+
+// console.log(characters[1]);
+// console.log(characters[2]);
+
+// var grades = [16, 20, 4];
+
+
+
+// Authentication thing 
+// a user in our
+// var user = {
+//     username: "collinvdv",
+//     password: "password123",
+//     age: 30
+// };
+
+// console.log(user.username);
+
+
+// var users = [
+//     {
+//         username: "collinvdv",
+//         password: "password123",
+//         age: 30,
+//         isMarried: false,
+//         animals: [
+//             "goats",
+//             "chicken",
+//             "sheep",
+//             0,
+//             true
+//         ]
+//     },
+//     {
+//         username: "jebus",
+//         password: "heavenisaplaceonearth",
+//         age: 100,
+//         isMarried: false
+//     }
+// ];
+
+// console.log(users);
+// console.log(users[0].password);
+
+
+
+/*
+* Operators 
+*/
+
+// var number1 = 10;
+// var number2 = 20;
+// var lifeIsBeautiful = true;
+
+// console.log(number1 + number2); // add
+// console.log(number1 - number2); // substract
+// console.log(number1 / number2); // divide
+// console.log(number1 * number2); // multiple
+// console.log(number1 % number2); // remainder
+
+// number1++;
+// number1 = number1 + 1;
+// number1 += 1;
+
+// number1 *= 2;
+// number1 = number1 * 2;
+
+// if (number1 < number2 || lifeIsBeautiful) {
+//     console.log("it is smaller");
+// }
+
+// if (number1 <= number2) {
+//     console.log("it is smaller and equal");
+// }
+
+// if (number1 > number2) {
+//     console.log("it is bigger than");
+// }
+
+// if (number1 >= number2) {
+//     console.log("it is bigger than");
+// }
+
+// if (number1 == number2) {
+//     console.log("it is equal to");
+// }
+
+// if (number1 != number2) {
+//     console.log("it is not equal to");
+// } else if () {
+//    // 
+// } else {
+//     ///
+// }
+
+// switch () {
+//     case: 
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+
+
+var number1 = 5;
+
+switch (number1) {
+    case "5":
+        console.log("It doesn't care about the type either in a switch case");
+        break;
+    default:
+        console.log("it does pay attention to the type in a switch");
+        break;
+}
+
+
